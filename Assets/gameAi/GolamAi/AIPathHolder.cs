4be59895ea5501eaf2c09361143c5560ec6f1d4c
Techmarine117 <keegan.strickler@gmail.com>
2021-10-30 14:35:38 +0400

    added more of my scripts to my namespace

--- Assets/gameAi/GolamAi/AIPathHolder.cs
+++ Assets/gameAi/GolamAi/AIPathHolder.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIPathHolder : MonoBehaviour
+namespace Brendan
 {
-    public List<Node> shortestPath;
-    
+    public class AIPathHolder : MonoBehaviour
+    {
+        public List<Node> shortestPath;
+
+    }
+
 }
