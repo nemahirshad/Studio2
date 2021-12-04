59c1e70b56307df773304b39c7cd2773fe820463
Danjer Snek <viperrules4ever@gmail.com>
2021-12-04 17:12:14 +0400

    Added Flocking System
    ---------------
    Progress Log:
    
    -Added FlockTest Scene.
    -Added Flocking System.
    -Flock System's Cohesion Behavior Is Functional.
    -Flock System's Alignment Behavior Is Functional.
    -Flock System's Avoidance Behavior Is Functional.
    -Flock System's StayInFlock Behavior Is Functional.
    ---------------
    Issues:
    
    -Flock System's Follow Leader Behavior Is Not Functional.
    ---------------
    Details:
    
    The Flock System is capable of creating Agents that can join together into groups without clipping into each other and occupying the same world space. These Agents will not go beyond a certain distance from the center of their parent object, i.e., the main Flock Object. However, issues arise when we try to get these Agents to form a single group and follow a Leader Object. Therefore, the Follow Leader Behavior has been disabled for the forseeable future, unless we can find a solution.

---
+++ Assets/Scripts/Flock System/FlockManager.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    //----------Private Variables----------
+
+    [SerializeField] GameObject agentPrefab;
+
+    public List<GameObject> flockAgents;
+
+    [SerializeField, Range(4, 50)]
+    int initialAgentCount = 10;
+
+    
+
+    //[SerializeField, Range(0f, 1f)]
+    //float movementMultiplier = 0.5f;
+
+    float movementSpeed;
+
+    //----------Public Variables----------
+
+    [Range(1f, 10f)]
+    public float spawnradius = 5;
+
+    [Range(0f, 10f)]
+    public float neighborRadius = 1.5f;
+
+    [Range(0f, 10f)]
+    public float avoidanceradius = 0.5f;
+
+    [Range(1f, 100f)]
+    public float speed = 5;
+
+    public GameObject leaderPrefab;
+    
+    public float cohesionWeight;
+    public float avoidanceWeight;
+    public float alignmentWeight;
+    public float maintainFlockWeight;
+
+    float followWeight;
+
+    bool needsLeader = false;
+
+    void Start()
+    {
+        flockAgents = new List<GameObject>();
+
+        //movementSpeed = speed * movementMultiplier;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            SpawnFlock();
+    }
+
+    public void SpawnFlock()
+    {
+        /*if (needsLeader)
+        {
+            GameObject leader = Instantiate(leaderPrefab, new Vector3(transform.position.x, 0, transform.position.z + spawnradius), Quaternion.identity, transform);
+            leader.name = gameObject.name + " Leader";
+
+            Vector2 randomIntCircle = Random.insideUnitCircle;
+
+            for (int i = 0; i < initialAgentCount; i++)
+            {
+                flockAgents.Add(Instantiate(agentPrefab,
+                                //new Vector3(randomIntCircle.x * spawnradius, 0, randomIntCircle.y * spawnradius),
+                                new Vector3(Random.insideUnitSphere.x * spawnradius, 0, Random.insideUnitSphere.z * spawnradius),
+                                Quaternion.identity,
+                                transform
+                                ));
+
+                flockAgents[i].name = agentPrefab.name + " " + i;
+                flockAgents[i].GetComponent<FlockAgent>().Initialize(flockAgents, this, /*transform.position,
+                                                                     leaderPrefab, speed, spawnradius,
+                                                                     cohesionWeight, avoidanceWeight,
+                                                                     alignmentWeight, followWeight,
+                                                                     maintainFlockWeight, true);
+            }
+        }*/
+
+        if (!needsLeader)
+        {
+            Vector2 randomIntCircle = Random.insideUnitCircle;
+
+            for (int i = 0; i < initialAgentCount; i++)
+            {
+                flockAgents.Add(Instantiate(agentPrefab,
+                                new Vector3(Random.insideUnitSphere.x * spawnradius, 0, Random.insideUnitSphere.z * spawnradius),
+                                Quaternion.identity,
+                                transform
+                                ));
+
+                flockAgents[i].name = agentPrefab.name + " " + i;
+                flockAgents[i].GetComponent<FlockAgent>().Initialize(flockAgents, this, /*transform.position,
+                                                                     leaderPrefab, speed, spawnradius,*/
+                                                                     cohesionWeight, avoidanceWeight,
+                                                                     alignmentWeight, followWeight,
+                                                                     maintainFlockWeight, false);
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 1, 0.25f);
+        Gizmos.DrawSphere(transform.position, spawnradius);
+    }
+}
